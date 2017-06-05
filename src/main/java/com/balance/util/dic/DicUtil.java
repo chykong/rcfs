1f6c9a0e5753350b988b87cf0d41ca33bdab654b
chykong <chykong@163.com>
2017-06-05 17:28:24 +0800

    代码恢复

--- src/main/java/com/balance/util/dic/DicUtil.java
+++ src/main/java/com/balance/util/dic/DicUtil.java
@@ -17,14 +17,14 @@ public class DicUtil {
      *
      * @return
      */
-    public List<ComboboxVO> listProjectProgress() {
+    public static List<ComboboxVO> listProjectProgress() {
         List<ComboboxVO> list = new ArrayList<>();
         ComboboxVO vo1 = new ComboboxVO("1", "前期准备阶段");
-        ComboboxVO vo2 = new ComboboxVO("1", "入户调查阶段");
-        ComboboxVO vo3 = new ComboboxVO("1", "预分方案制作阶段");
-        ComboboxVO vo4 = new ComboboxVO("1", "动迁准备阶段");
-        ComboboxVO vo5 = new ComboboxVO("1", "动迁阶段");
-        ComboboxVO vo6 = new ComboboxVO("1", "收尾阶段");
+        ComboboxVO vo2 = new ComboboxVO("2", "入户调查阶段");
+        ComboboxVO vo3 = new ComboboxVO("3", "预分方案制作阶段");
+        ComboboxVO vo4 = new ComboboxVO("4", "动迁准备阶段");
+        ComboboxVO vo5 = new ComboboxVO("5", "动迁阶段");
+        ComboboxVO vo6 = new ComboboxVO("6", "收尾阶段");
         list.add(vo1);
         list.add(vo2);
         list.add(vo3);

66a61d6d3864b079e57e9e29d8923e22f2b01f82
lixm <Administrator@PC-201603151605>
2017-06-05 12:36:00 +0800

    项目简介

--- src/main/java/com/balance/prj/model/PrjBaseinfo.java
+++ src/main/java/com/balance/prj/model/PrjBaseinfo.java
@@ -16,21 +16,15 @@ public class PrjBaseinfo {
     private String last_modified_by;  //最后更改人
     private Date created_at;  //创建时间
     private String created_by;  //创建人
+    private String type;//类型 1 项目简介   2 工作流程 3 组织架构
 
     @Override
-    public String toString() {
-        return "PrjBaseinfo{" +
-                "id=" + id +
-                ", prj_name='" + prj_name + '\'' +
-                ", introduction='" + introduction + '\'' +
-                ", flow='" + flow + '\'' +
-                ", architecture='" + architecture + '\'' +
-                ", last_modified_at=" + last_modified_at +
-                ", last_modified_by='" + last_modified_by + '\'' +
-                ", created_at=" + created_at +
-                ", created_by='" + created_by + '\'' +
-                '}';
-    }
+	public String toString() {
+		return "PrjBaseinfo [id=" + id + ", prj_name=" + prj_name + ", introduction=" + introduction + ", flow=" + flow
+				+ ", architecture=" + architecture + ", last_modified_at=" + last_modified_at + ", last_modified_by="
+				+ last_modified_by + ", created_at=" + created_at + ", created_by=" + created_by + ", type=" + type
+				+ "]";
+	}
 
     public int getId() {
         return id;
@@ -103,4 +97,13 @@ public class PrjBaseinfo {
     public void setCreated_by(String created_by) {
         this.created_by = created_by;
     }
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+    
 }

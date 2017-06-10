b98d10b1cea1e48b71053d177617ac80624884cc
chykong <chykong@163.com>
2017-06-10 10:37:52 +0800

    修改uedtor的显示按钮

--- src/main/webapp/assets/js/ueditorToolbar.js
+++ src/main/webapp/assets/js/ueditorToolbar.js
@@ -1,6 +1,23 @@
-var ueditorToolbar1=[['fullscreen', 'source', 'undo', 'redo'],
+/*var ueditorToolbar1=[['fullscreen', 'source', 'undo', 'redo'],
                                           ['bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript',
                                               'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain',
                                               '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall',
-                                              'insertimage', 'cleardoc']
+                                              'insertimage', 'cleardoc',
+                                               'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
+                                              ]
+                                      ]*/
+
+                                      var ueditorToolbar1=[[
+                                                                       'fullscreen', 'source', '|', 'undo', 'redo', '|',
+                                                                       'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+                                                                       'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
+                                                                       'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
+                                                                       'directionalityltr', 'directionalityrtl', 'indent', '|',
+                                                                       'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
+                                                                       'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+                                                                       'simpleupload', 'insertimage',  'attachment',   'pagebreak',  'background', '|',
+                                                                       'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
+                                                                       'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
+                                                                       'print', 'preview', 'searchreplace'
+                                                                   ]
                                       ]

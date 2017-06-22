0ae8f9d855143aa7045e1bac9fc338acdde77ed9
chykong <chykong@163.com>
2017-06-22 18:29:41 +0800

    解决api跨域

--- src/main/java/com/balance/config/interceptor/AjaxAccessInterceptor.java
+++ src/main/java/com/balance/config/interceptor/AjaxAccessInterceptor.java
@@ -21,7 +21,7 @@ public class AjaxAccessInterceptor implements HandlerInterceptor {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", " Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", " Origin, X-Requested-With, Content-Type, Accept,Authorization");
         return true;
     }
 
